66414a586900e83bc990c3774365e17f3a10efe2
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-08-20 00:00:26 -0400

    Cells are alive

--- game-of-life/src/components/Game.js
+++ game-of-life/src/components/Game.js
@@ -6,6 +6,15 @@ const cell_size = 20;
 const width = 800;
 const height = 600;
 
+class Cell extends Component {
+  render() {
+    const { x, y } = this.props;
+
+    return (
+      <div className="cell" style={{ left: `${cell_size * x + 1}px`, top: `${cell_size * y + 1}px`, width: `${cell_size - 1}px`, height: `${cell_size - 1}px`}} />
+    )
+  }
+}
 class Game extends Component {
   constructor() {
     super();
@@ -29,6 +38,7 @@ class Game extends Component {
     return board;
   }
 
+  // Generates the cell list from the board state
   makeCells() {
     let cells = [];
     for (let y = 0; y < this.rows; y++) {
@@ -40,12 +50,45 @@ class Game extends Component {
     }
     return cells;
   }
+
+  // Calculates the position of the board element
+  getElementOffset() {
+    const rect = this.boardRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: (rect.left + window.pageXOffset) - doc.clientLeft,
+      y: (rect.top + window.pageYOffset) - doc.clientTop,
+    };
+  }
+
+  // Retrieve the click position of the board element
+  handleClick = e => {
+    const elemOffset = this.getElementOffset();
+
+    const offsetX = e.clientX - elemOffset.x;
+    const offsetY = e.clientY - elemOffset.y;
+
+    const x = Math.floor(offsetX/cell_size);
+    const y = Math.floor(offsetY/cell_size);
+
+    if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+      this.board[y][x] = !this.board[y][x];
+    }
+
+    this.setState({ cells: this.makeCells() });
+  }
   
   render() {
+    const { cells } = this.state;
+
     return (
       <div>
         <div className="board" style={{ width: width, height: height, backgroundSize: `${cell_size}px ${cell_size}px`}} onClick={this.handleClick} ref={e => {this.boardRef = e;}}>
-
+          {cells.map(cell => {
+            return (
+            <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`} />
+          )})}
         </div>
       </div>
     )
