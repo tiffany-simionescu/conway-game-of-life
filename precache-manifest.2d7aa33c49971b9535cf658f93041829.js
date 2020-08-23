45412086f68b2663c1c0259a9ba820ee9485d26c
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-08-22 23:30:15 -0400

    Updates

---
+++ precache-manifest.2d7aa33c49971b9535cf658f93041829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2376464f6f136da7b152ba5ec39a3a3e",
+    "url": "/conway-game-of-life/index.html"
+  },
+  {
+    "revision": "cf1dae6556982846f084",
+    "url": "/conway-game-of-life/static/css/main.4451a55f.chunk.css"
+  },
+  {
+    "revision": "4ec7826eb1b5a2cd20a9",
+    "url": "/conway-game-of-life/static/js/2.acd6da9d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/conway-game-of-life/static/js/2.acd6da9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1dae6556982846f084",
+    "url": "/conway-game-of-life/static/js/main.401c1930.chunk.js"
+  },
+  {
+    "revision": "7e6fb7af2d037d24ec98",
+    "url": "/conway-game-of-life/static/js/runtime-main.8cbe773b.js"
+  }
+]);
